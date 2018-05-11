e18fb535c23d893d06c616df76c70c0214903f23
Condon Wang <condonwang@augmentum.com.cn>
2018-05-11 16:46:44 +0800

    Add staging host

--- api/config.js
+++ api/config.js
@@ -17,8 +17,8 @@ const qa = {
 const staging = {
   trade: "https://xapi-staging.alct56.com/api/v1/trade",
   driver: "https://dapi-staging.alct56.com/api/v1/driver",
-  miniprogram: 'http://192.168.111.211:5000/api/v1/miniprogram',
-  image: 'http://192.168.111.211:5000/api/v1/miniprogram/images',
+  miniprogram: 'https://mp-staging.alct56.com/api/v1/miniprogram',
+  image: 'https://mp-staging.alct56.com/api/v1/miniprogram/images',
   agreement: "https://x-staging.alct56.com/mobile/Agreement.html"
 }
 

e89f84af5641172ccd57fcd23dac31624d6ac048
Condon Wang <condonwang@augmentum.com.cn>
2018-05-15 11:01:48 +0800

    Refine lib check.

--- app.js
+++ app.js
@@ -2,13 +2,17 @@ App({
 
   onShow: function (options) {
     
-    // 接口兼容性检测
+    /**
+     * 接口兼容性检测
+     * 基础库 1.9.90 开始支持接口： wx.getUpdateManager
+     * 接口wx.getUpdateManager是当前程序基础库版本要求最高的，故检测到设备兼容该接口即可
+     */
     if (wx.getUpdateManager) {
       this.updateMiniprogram();
     } else {
       wx.showModal({
         title: '温馨提示',
-        content: '当前微信版本过低，无法立即应用更新，请升级到最新微信版本后重试。',
+        content: '当前微信版本过低，建议升级到最新版本的微信后再使用本程序',
         showCancel: false,
         confirmText: '知道了'
       })

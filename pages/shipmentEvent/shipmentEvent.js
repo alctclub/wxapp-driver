78cf0b699f28b15530aebfd98adc07a1adb546ac
Condon Wang <condonwang@augmentum.com.cn>
2018-06-04 10:55:06 +0800

    Bug #3938 【微信小程序】到货页面，到货成功后，显示 图片上传失败

--- pages/shipmentEvent/shipmentEvent.js
+++ pages/shipmentEvent/shipmentEvent.js
@@ -53,39 +53,40 @@ Page({
       formId: event.detail.formId
     })
 
-    that.checkNetwork();
-
-    if (order.statusCode === 50) {
-      that.getLocationWithoutCheck();
-    } else {
-      that.checkLocationPermission();
-    }
-    
-  },
+    that.checkNetwork().then(() => {
+      if (order.statusCode === 50) {
+        that.getLocationWithoutCheck();
+      } else {
+        that.checkLocationPermission();
+      }
+    }).catch(() => {
+      wx.hideLoading();
+      wx.showModal({
+        content: '由于网络或其它原因导致系统异常，请检查后重试',
+        showCancel: false,
+        confirmText: '确定'
+      })
+    });
+  }, 
 
   checkNetwork: function () {
-    wx.getNetworkType({
-      success: function (res) {
-        // 返回网络类型, 有效值：
-        // wifi/2g/3g/4g/unknown(Android下不常见的网络类型)/none(无网络)
-        var networkType = res.networkType
-        if (networkType === 'none') {
-          wx.hideLoading();
-          wx.showModal({
-            content: '由于网络或其它原因导致系统异常，请检查后重试',
-            showCancel: false,
-            confirmText: '确定'
-          })
+    return new Promise((resolve, reject) => {
+        wx.getNetworkType({
+        success: function (res) {
+          // 返回网络类型, 有效值：
+          // wifi/2g/3g/4g/unknown(Android下不常见的网络类型)/none(无网络)
+          var networkType = res.networkType
+          if (networkType === 'none') {
+            reject();
+          } else {
+            resolve();
+          }
+        },
+        
+        fail: function () {
+          reject();
         }
-      },
-      fail: function () {
-        wx.hideLoading();
-        wx.showModal({
-          content: '由于网络或其它原因导致系统异常，请检查后重试',
-          showCancel: false,
-          confirmText: '确定'
-        })
-      }
+      })
     })
   },
 

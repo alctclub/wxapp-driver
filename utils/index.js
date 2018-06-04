2ef8a3e96c2903d8f862f84a5900fd3ecf3ed35c
Condon Wang <condonwang@augmentum.com.cn>
2018-06-04 16:08:00 +0800

    Remove debugger

--- utils/index.js
+++ utils/index.js
@@ -122,13 +122,11 @@ export function transformToServerTime(date, formate) {
 }
 
 export function checkNetwork() {
-  debugger
   return new Promise((resolve, reject) => {
     wx.getNetworkType({
       success: function (res) {
         // 返回网络类型, 有效值：
         // wifi/2g/3g/4g/unknown(Android下不常见的网络类型)/none(无网络)
-        debugger
         if (res && res.networkType === 'none') {
           reject();
         } else {
